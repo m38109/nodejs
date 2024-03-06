3982c25bca501aebba874e449f214f21ed6b36a0
m38109 <m38109@gmail.com>
2024-03-06 10:39:06 +0800

    add view available courses

--- app.js
+++ app.js
@@ -71,12 +71,10 @@ app.put('/assignTeacher/:id', (req, res) => {
 app.put('/courseAvailability/:id', (req, res) => {
     const Course_upid = req.params.id;
     const isAvailable = req.body.isAvailable;
-    const UserID= req.body.UserID;
+    const UserID = req.body.UserID;
 
     //verify user's role
-    const checkAdminSql = `SELECT RoleID
-                           FROM users
-                           WHERE UserID = ?`;
+    const checkAdminSql = `SELECT RoleID FROM users WHERE UserID = ?`;
 
     connection.query(checkAdminSql, [UserID], (error, results) => {
         if (error) {
@@ -111,8 +109,30 @@ app.put('/courseAvailability/:id', (req, res) => {
     });
 });
 
+// View all available courses with teacher's name
+app.get('/viewcourses/',(req, res) => {
+
+    const AvailableCourseSql = `SELECT 
+        courses.Title AS CourseTitle, users.Name AS TeacherName
+        FROM courses
+        JOIN users ON courses.TeacherID = users.UserID
+        WHERE courses.isAvailable = 1`
+
+    connection.query(AvailableCourseSql, (error, results) => {
+        if (error) {
+            res.status(500).send('Fail to list the courses.');
+            throw error;
+        }
+        res.status(200).json(results)
+
+    });
+
+});
+
+// Student enroll in available course once
+
 
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}.`);
+    console.log(`Server is running on port ${port}.`);
 });
